b7f732796e017da1be159e8ef4d8810f3a5c37c6
Sjoshi7 <sjoshise7@gmail.com>
2019-08-01 01:05:27 +0530

    signup validator and destructuring

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const {getPosts, createPost} = require("../controller/post");
-const validator = require("../validator");
+const {createPostValidator} = require("../validator");
 
 const router = express.Router();
 
 router.get('/',getPosts);
 //checks for validation before going to create post
-router.post('/post', validator.createPostValidator, createPost);
+router.post('/post', createPostValidator, createPost);
 
 module.exports = router;
--- validator/index.js
+++ validator/index.js
@@ -1,4 +1,4 @@
-exports.createPostValidator = (req,res)=>{
+exports.createPostValidator = (req,res,next)=>{
 	//title
 	req.check("title","Write a Title").notEmpty();
 	req.check("title","Title must be between 4-150 characters").isLength({
@@ -19,5 +19,32 @@ exports.createPostValidator = (req,res)=>{
 		return res.status(400).json({error: firstError})
 	}
 	//proceed to next middleware
+	next();
+}
+
+exports.userSignupValidator = (req,res,next) => {
+	//name is not null and b/w 4-20 char
+	req.check("name","Name is required").notEmpty();
+	//email
+	req.check("email","Email must be between 3-32 characters")
+	.matches(/.+@.+\..+/)
+	.withMessage("Email must contain @")
+	.isLength({
+		min:4,
+		max:2000 
+	})
+	//check for password
+	req.check("password","Password is required").notEmpty();
+	req.check('password')
+	.isLength({	min:6})
+	.withMessage("Password must contain 6 characters")
+	.matches(/\d/)
+	.withMessage("Password must contain a number")
+	//check for erros
+	const errors = req.validationErrors();
+	if(errors){
+		const firstError = errors.map(error=>error.msg)[0];
+		return res.status(400).json({error:firstError});
+	}
 	next();
 }
