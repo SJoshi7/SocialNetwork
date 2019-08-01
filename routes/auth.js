b13743b07769551966058db5dcb0730eee1435e2
Sjoshi7 <sjoshise7@gmail.com>
2019-08-01 23:51:59 +0530

    implemented authorization

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {signup, signin, signout} = require("../controller/auth");
+const {userById} = require('../controller/user');
 const {userSignupValidator} = require("../validator");
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.post('/signup',userSignupValidator,signup);
 router.post('/signin',signin);
 router.get('/signout',signout);
+//if any route will contain :userId, our app will first execute userId()
+router.param('userId', userById)
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {getPosts, createPost} = require("../controller/post");
+const {userById} = require("../controller/user");
 const {createPostValidator} = require("../validator");
 const {requireSignin} = require('../controller/auth');
 
 const router = express.Router();
 
-router.get('/',requireSignin, getPosts);
+router.get('/', getPosts);
 //checks for validation before going to create post
-router.post('/post', createPostValidator, createPost);
+router.post('/post', requireSignin, createPostValidator, createPost);
+//if any route will contain :userId, our app will first execute userId()
+router.param('userId', userById)
 
 module.exports = router;
